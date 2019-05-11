9d87761bf4635afd5b5c2058c15ec9fa446f797c
bappyasif <bappyasif@hotmail.com>
2019-05-11 22:27:45 +0600

    Few Examples Using Test Listeners
    And Few Reports On Running Those Test Suites and Test Suite Collections.

---
+++ Test Listeners/NewTestListenerExample.groovy
@@ -0,0 +1,103 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+
+import internal.GlobalVariable as GlobalVariable
+
+import com.kms.katalon.core.annotation.BeforeTestCase
+import com.kms.katalon.core.annotation.BeforeTestSuite
+import com.kms.katalon.core.annotation.AfterTestCase
+import com.kms.katalon.core.annotation.AfterTestSuite
+import com.kms.katalon.core.context.TestCaseContext
+import com.kms.katalon.core.context.TestSuiteContext
+
+class NewTestListenerExample {
+	
+	/**
+	 * Executes before every test case starts.
+	 * @return
+	 */
+	@BeforeTestCase
+	def myBeforeTestSample(){
+		println ("  Inside myBeforeTestSample  ")
+	}
+	
+	/**
+	 * Executes before every test case starts.
+	 * @param testCaseContext related information of the executed test case.
+	 */
+	@BeforeTestCase
+	def sampleBeforeTestCase(TestCaseContext testCaseContext) {
+		println testCaseContext.getTestCaseId()
+		println testCaseContext.getTestCaseVariables()
+	}
+
+	/**
+	 * Executes after every test case ends.
+	 * @return
+	 */
+	@AfterTestCase
+	def myAfterTestSample(){
+		println ("  Inside myAfterTestSample  ")
+	}
+	
+	
+	/**
+	 * Executes after every test case ends.
+	 * @param testCaseContext related information of the executed test case.
+	 */
+	@AfterTestCase
+	def sampleAfterTestCase(TestCaseContext testCaseContext) {
+		println testCaseContext.getTestCaseId()
+		println testCaseContext.getTestCaseStatus()
+	}
+
+	/**
+	 * Executes before every test suite starts.
+	 * @return
+	 */
+	@BeforeTestSuite
+	def myBeforeTestSuite(){
+		println ("  Inside myBeforeTestSuite  ")
+	}
+	
+	
+	/**
+	 * Executes before every test suite starts.
+	 * @param testSuiteContext: related information of the executed test suite.
+	 */
+	@BeforeTestSuite
+	def sampleBeforeTestSuite(TestSuiteContext testSuiteContext) {
+		println testSuiteContext.getTestSuiteId()
+	}
+
+	/**
+	 * Executes after every test suite ends.
+	 * @return
+	 */
+	@AfterTestSuite
+	def myAfterTestSuite(){
+		println ("  Inside myAfterTestSuite  ")
+	}
+	
+	
+	/**
+	 * Executes after every test suite ends.
+	 * @param testSuiteContext: related information of the executed test suite.
+	 */
+	@AfterTestSuite
+	def sampleAfterTestSuite(TestSuiteContext testSuiteContext) {
+		println testSuiteContext.getTestSuiteId()
+	}
+}
---
+++ Test Suites/Listeners Test/Test Listeners Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Listeners Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ea0dfd5c-98e8-4f2d-aa5b-1a181f76d8aa</testSuiteGuid>
+   <testCaseLink>
+      <guid>2d7820a0-3e15-4035-a6f7-f8d2df049b4a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestNG Listeners/Trying Listeners</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
